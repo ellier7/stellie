50f568fd34d4d0d8945b6a235c0f6a84ae342be9
Ellie Roussopoulos <ellie@Ellies-Air.fios-router.home>
2022-08-16 21:33:12 -0400

    First commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,14 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [{
+    resolve: "gatsby-plugin-web-font-loader",
+    options: {
+      typekit: {
+        id: process.env.TYPEKIT_ID,
+      },
+    },
+  },],
 }
 
 export default config
---
+++ src/css/main.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PageStyles = styled.div` 
+color: #006167;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+font-family: "Galada";
+font-size: 30px;
+font-weight: 500;
+`
